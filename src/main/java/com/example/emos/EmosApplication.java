ac71dfe6ce2d6e51a50e101a23822f11258812e7
yyy <2484218565@qq.com>
2023-02-12 20:08:14 +0800

    1.1create project

---
+++ src/main/java/com/example/emos/EmosApplication.java
@@ -0,0 +1,13 @@
+package com.example.emos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosApplication.class, args);
+    }
+
+}
