6f0031b4cef93960cf808c21448927e6fa56013d
yyy <2484218565@qq.com>
2023-02-12 20:50:54 +0800

    1.4 封装Web返回对象

--- src/main/java/com/example/emos/wx/common/utils/R.java
+++ src/main/java/com/example/emos/wx/common/utils/R.java
@@ -4,22 +4,39 @@ import org.apache.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
-//
+//封装web返回对象
+//对后端处理后的数据进行封装，以一个统一格式返回给客户端
+//封装后的主要字符，{状态码，消息}{code，msg}
+//返回对象类型HashMap<String,Object>
 public class R extends HashMap<String,Object> {
     public R(){
         put("code", HttpStatus.SC_OK);
         put("msg","success");
     }
+    //ok方法和error方法共用
     public R put(String key,Object value){
         super.put(key,value);
         return this;
     }
+    //ok（）方法生成R对象实例
     public static R ok(){
         return new R();
+    }
+    public static R ok(int code,String msg){
+        R r=new R();
+        r.put("code",code);
+        r.put("msg",msg);
+        return r;
     }
     public static R ok(String msg){
         R r=new R();
         r.put("msg",msg);
+        return r;
+    }
+    //参数HttpStatus.SC_OK
+    public static R ok(int code){
+        R r=new R();
+        r.put("code",code);
         return r;
     }
     public static R ok(Map<String,Object> map){
@@ -27,6 +44,8 @@ public class R extends HashMap<String,Object> {
         r.putAll(map);
         return r;
     }
+
+    //失败的返回
 
     public static R error(int code,String msg){
         R r=new R();
