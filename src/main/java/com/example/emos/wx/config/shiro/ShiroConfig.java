49f1484d272e1bcb7195490e0bf700f13c10e0d9
yyy <2484218565@qq.com>
2023-02-13 16:25:06 +0800

    2.5 创建ShiroConfig类，将filter类和realm类配置到shiro框架

---
+++ src/main/java/com/example/emos/wx/config/shiro/ShiroConfig.java
@@ -0,0 +1,71 @@
+package com.example.emos.wx.config.shiro;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+    @Bean("securityManager")
+    public SecurityManager securityManager(OAuth2Realm oAuth2Realm){
+        DefaultSecurityManager defaultSecurityManager =new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(oAuth2Realm);
+        defaultSecurityManager.setRememberMeManager(null);
+        return defaultSecurityManager;
+
+    }
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager,OAuth2Realm oAuth2Realm){
+        ShiroFilterFactoryBean shiroFilter=new ShiroFilterFactoryBean();
+        shiroFilter.setSecurityManager(securityManager);
+        //向FactoryBean其中封装FilterBean对象
+        //OAuth过滤
+        Map<String, Filter>filters=new HashedMap();
+        filters.put("OAuth2",oAuth2Realm);//OAuth2Realm设置为“anon"不需要被拦截
+        shiroFilter.setFilters(filters);
+        //
+        Map<String,String>filterMap=new HashedMap();
+        filterMap.put("/webjars/**", "anon");
+        filterMap.put("/druid/**", "anon");
+        filterMap.put("/app/**", "anon");
+        filterMap.put("/sys/login", "anon");
+        filterMap.put("/swagger/**", "anon");
+        filterMap.put("/v2/api-docs", "anon");
+        filterMap.put("/swagger-ui.html", "anon");
+        filterMap.put("/swagger-resources/**", "anon");
+        filterMap.put("/captcha.jpg", "anon");
+        filterMap.put("/user/register", "anon");
+        filterMap.put("/user/login", "anon");
+        filterMap.put("/test/**", "anon");
+        filterMap.put("/meeting/recieveNotify", "anon");
+        filterMap.put("/**", "oauth2");
+        shiroFilter.setFilterChainDefinitionMap(filterMap);
+
+        return shiroFilter;
+
+
+
+        return shiroFilter;
+
+    }
+    @Bean("lifecycleBeanPostProcessor")
+    //生成生命周期管理对象
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor(){
+        return new LifecycleBeanPostProcessor();
+    }
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+    }
+    //利用切面类向客户端返回后端生成的新的令牌
+}
